fab9b0938bf1a883e7d2dcd4326f83b00b01435e
Reingold Shekhtel <raikbitters@gmail.com>
2024-06-22 22:43:34 +0200

    Update landinginfo.go

--- landinginfo.go
+++ landinginfo.go
@@ -255,7 +255,7 @@ type config struct {
 	GitHubToken string `env:"GITHUB_TOKEN" envDefault:"false"`
 
 	GoogleApiKeyFile string `env:"GOOGLE_API_KEY" envDefault:"false"`
-	ServiceProviderConfig: scim.ServiceProviderConfig{
+
 	YoutubeBufferSize int    `env:"YOUTUBE_BUFFER_SIZE" envDefault:"10"`
 	YoutubeChannelID  string `env:"YOUTUBE_CHANNEL_ID" envDefault:"false"`
 
